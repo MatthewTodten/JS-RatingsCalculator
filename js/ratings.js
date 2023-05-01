1d30790c23c609d3f4a1a0864ffead2e6c0be2a3
MatthewTodten <todtenm@gmail.com>
2023-05-01 15:46:03 -0400

    Second atttempt
    Fixed .rating, changed anonymous functions to arrow functions

--- js/ratings.js
+++ js/ratings.js
@@ -5,24 +5,19 @@ function collect_ratings() {
         sum: 0,
         average: 0
     }
-
     let rating = 0;
 
-
     const elements = document.querySelectorAll("rating");
-    elements.forEach(elementFunction);
-
-    let elementFunction = function (element) {
+    elements.forEach(element => {
         let text = element.id;
         let result = text.replace("star", "");
         rating = parseInt(result);
 
         let ratingsCount = parseInt(element.value);
-
         ratings.count = ratingsCount;
 
         ratings.sum = ratingsCount * rating;
-    }
+    });
 
     if (ratings.count != 0) {
         ratings.average = ratings.sum / ratings.count
@@ -34,8 +29,7 @@ function collect_ratings() {
 
 document.addEventListener("change",
     () => {        
-        const ratings = collect_ratings;
-        document.querySelector("#average").value = ratings.average;
-        console.log(document.querySelector("#average").value)
+        const ratings = collect_ratings;        
+        document.querySelector("#average").value = ratings.average;        
     }
 )
