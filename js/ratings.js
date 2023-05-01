d33be6759863fd03a351cb74e3e509544ce80517
MatthewTodten <todtenm@gmail.com>
2023-05-01 17:28:41 -0400

    Third attempt
    Still debugging. Its detecting the change but not displaying the average.

--- js/ratings.js
+++ js/ratings.js
@@ -7,7 +7,7 @@ function collect_ratings() {
     }
     let rating = 0;
 
-    const elements = document.querySelectorAll("rating");
+    const elements = document.querySelectorAll(".rating");
     elements.forEach(element => {
         let text = element.id;
         let result = text.replace("star", "");
