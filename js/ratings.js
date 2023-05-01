5cc14abad4a3ea3786993bd3afedaf1179cd3e9c
MatthewTodten <todtenm@gmail.com>
2023-05-01 15:23:17 -0400

    First attempt
    Likely issue with forEach

---
+++ js/ratings.js
@@ -0,0 +1,41 @@
+function collect_ratings() {
+    console.log("function started")
+    const ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    }
+
+    let rating = 0;
+
+
+    const elements = document.querySelectorAll("rating");
+    elements.forEach(elementFunction);
+
+    let elementFunction = function (element) {
+        let text = element.id;
+        let result = text.replace("star", "");
+        rating = parseInt(result);
+
+        let ratingsCount = parseInt(element.value);
+
+        ratings.count = ratingsCount;
+
+        ratings.sum = ratingsCount * rating;
+    }
+
+    if (ratings.count != 0) {
+        ratings.average = ratings.sum / ratings.count
+    }
+
+    console.log("function run")
+    return ratings
+}
+
+document.addEventListener("change",
+    () => {        
+        const ratings = collect_ratings;
+        document.querySelector("#average").value = ratings.average;
+        console.log(document.querySelector("#average").value)
+    }
+)
